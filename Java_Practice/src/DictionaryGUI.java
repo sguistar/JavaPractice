cf9d9f12e71908ee8416feb76f14f12f8a739bca
Lin Sean <120402926+sguistar@users.noreply.github.com>
2025-05-15 21:14:24 +0800

    Update DictionaryGUI.java

--- Java_Practice/src/DictionaryGUI.java
+++ Java_Practice/src/DictionaryGUI.java
@@ -57,8 +57,8 @@ public class DictionaryGUI extends JFrame {
 
     private String queryDatabase(String word) {
         String url = "jdbc:mysql://rm-bp13z1291yal195860o.mysql.rds.aliyuncs.com:3306/book?useSSL=false&serverTimezone=GMT";
-        String user = "root";
-        String pass = "Giti#73920";
+        String user = "mysql_username";
+        String pass = "your_password";
         String meaning = null;
 
         try {
